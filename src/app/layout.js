05964d8edfe254d95ac236bfe21991a7c9bf5b40
juhairulislam <juhairulislam2018@gmail.com>
2026-04-24 21:24:38 +0600

    project initialized

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@heroui/react";
+import Navbar from "@/Components/Shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,15 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+        <Header>
+
+        </Header>
+
+        <Navbar></Navbar>
+        
+        {children}</body>
     </html>
   );
 }
