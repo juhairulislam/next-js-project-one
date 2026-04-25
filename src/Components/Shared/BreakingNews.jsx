9ffdd7984a5008b94d6033e7300587acb448a655
juhairulislam <juhairulislam2018@gmail.com>
2026-04-25 10:46:28 +0600

    add news data dynamically

--- src/Components/Shared/BreakingNews.jsx
+++ src/Components/Shared/BreakingNews.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
+
+const news = [
+  {
+    "id": 1,
+    "title": "Bangladesh Launches Nationwide Digital Education Program."
+  },
+  {
+    "id": 2,
+    "title": "Global Tech Leaders Announce Breakthrough in AI Innovation."
+  },
+  {
+    "id": 3,
+    "title": "Economic Growth Shows Positive Trend in South Asia."
+  },
+  {
+    "id": 4,
+    "title": "New Environmental Policy Targets Climate Change Impact."
+  },
+  {
+    "id": 5,
+    "title": "Youth Entrepreneurship on the Rise in Urban Areas."
+  }
+]
+
 const BreakingNews = () => {
     return (
-        <div>
+        <div className='flex justify-between gap-4 items-center bg-gray-200 p-4 w-[85%] mx-auto'>
+
+            <button className='btn bg-pink-500 text-white'>Breaking News</button>
             <Marquee>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ad soluta saepe modi doloremque et, vitae ea itaque ratione architecto reiciendis provident sint neque ut fugiat aut autem nesciunt voluptatum harum quaerat maxime. Sit, repellendus est! Quibusdam, distinctio. Quisquam exercitationem perferendis, magnam iusto cumque, adipisci architecto ratione commodi dolores cupiditate, voluptas odit laudantium animi assumenda perspiciatis in voluptate! Officia, repellat! Eligendi quisquam quas at dolorum officiis, voluptates voluptatibus reiciendis aperiam fuga maxime sit. Ipsam maxime eaque unde laborum, ullam officia debitis temporibus. Temporibus impedit est veritatis aut molestias, dolores inventore iure consequuntur incidunt itaque possimus! Iusto suscipit consectetur sit beatae dolores voluptas consequatur possimus, ut blanditiis, cupiditate quibusdam praesentium fugiat nam maxime, enim recusandae. Quibusdam veniam id, fugit molestiae ad ex! Odit quasi quibusdam eveniet laborum consequatur autem natus eius itaque velit labore dicta error impedit suscipit deserunt, incidunt cupiditate optio officiis soluta accusantium eos, aut molestiae aspernatur. Molestias error voluptatem est ea maiores sed temporibus placeat, nam animi fugit officia quia vitae, itaque iusto debitis culpa eaque aut, distinctio modi laborum praesentium dolore veniam doloremque. Reiciendis mollitia nobis iste corrupti culpa nihil aut amet expedita quo. Impedit provident deleniti doloremque vitae minima ullam quis rerum, quas nemo amet quia, aliquam ut quibusdam facilis fugit dicta tempore nam explicabo quae aliquid recusandae iure cum odit! Ullam quia quas, est impedit doloremque architecto deserunt ducimus iure recusandae! Voluptatem ipsum, vitae laboriosam molestias praesentium molestiae repudiandae vero perferendis, quasi, sed voluptatibus facere corrupti. Rerum esse a atque animi fuga ad illum deserunt!
+               {
+                news.map(n => <span key={n.id} className='mx-8'>
+                    {n.title}
+
+                </span>)
+               }
             </Marquee>
             
         </div>
