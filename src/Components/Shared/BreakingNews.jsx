aca21f6ded86d5303ef4ffccd261a6f5f32cdf1b
juhairulislam <juhairulislam2018@gmail.com>
2026-04-25 10:31:57 +0600

    add marquee

---
+++ src/Components/Shared/BreakingNews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const BreakingNews = () => {
+    return (
+        <div>
+            <Marquee>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ad soluta saepe modi doloremque et, vitae ea itaque ratione architecto reiciendis provident sint neque ut fugiat aut autem nesciunt voluptatum harum quaerat maxime. Sit, repellendus est! Quibusdam, distinctio. Quisquam exercitationem perferendis, magnam iusto cumque, adipisci architecto ratione commodi dolores cupiditate, voluptas odit laudantium animi assumenda perspiciatis in voluptate! Officia, repellat! Eligendi quisquam quas at dolorum officiis, voluptates voluptatibus reiciendis aperiam fuga maxime sit. Ipsam maxime eaque unde laborum, ullam officia debitis temporibus. Temporibus impedit est veritatis aut molestias, dolores inventore iure consequuntur incidunt itaque possimus! Iusto suscipit consectetur sit beatae dolores voluptas consequatur possimus, ut blanditiis, cupiditate quibusdam praesentium fugiat nam maxime, enim recusandae. Quibusdam veniam id, fugit molestiae ad ex! Odit quasi quibusdam eveniet laborum consequatur autem natus eius itaque velit labore dicta error impedit suscipit deserunt, incidunt cupiditate optio officiis soluta accusantium eos, aut molestiae aspernatur. Molestias error voluptatem est ea maiores sed temporibus placeat, nam animi fugit officia quia vitae, itaque iusto debitis culpa eaque aut, distinctio modi laborum praesentium dolore veniam doloremque. Reiciendis mollitia nobis iste corrupti culpa nihil aut amet expedita quo. Impedit provident deleniti doloremque vitae minima ullam quis rerum, quas nemo amet quia, aliquam ut quibusdam facilis fugit dicta tempore nam explicabo quae aliquid recusandae iure cum odit! Ullam quia quas, est impedit doloremque architecto deserunt ducimus iure recusandae! Voluptatem ipsum, vitae laboriosam molestias praesentium molestiae repudiandae vero perferendis, quasi, sed voluptatibus facere corrupti. Rerum esse a atque animi fuga ad illum deserunt!
+            </Marquee>
+            
+        </div>
+    );
+};
+
+export default BreakingNews;
--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,3 +1,4 @@
+import BreakingNews from '@/Components/Shared/BreakingNews';
 import Header from '@/Components/Shared/Header';
 import Navbar from '@/Components/Shared/Navbar';
 import React from 'react';
@@ -6,6 +7,7 @@ const MainLayout = ({children}) => {
     return (
         <>
                <Header></Header>
+               <BreakingNews></BreakingNews>
         <Navbar></Navbar>
         {children}
         </>
