573934de7d701c87ee6c0fefe7105a23142c3a36
juhairulislam <juhairulislam2018@gmail.com>
2026-04-26 18:09:34 +0600

    add redirect at home page

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,78 +1,10 @@
-import LeftSideBar from "@/Components/homepage/news/LeftSideBar";
-import RightSideBar from "@/Components/homepage/news/RightSideBar";
-import Image from "next/image";
+import { redirect } from "next/navigation";
+ 
 
+const default_id = '01'
 
-const Categories =async () =>{
-
-
-  const res =await fetch('https://openapi.programming-hero.com/api/news/categories') ;
-
-  const data =await res.json() ;
-
-  return data.data ;
-
-}
-const newsCategoriesById =async (category_id) =>{
-
-
-  const res =await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`) ;
-
-  const data =await res.json() ;
-
-  return data.data ;
-
-}
 
 export default async function Home() {
 
-  const categories =await Categories()
-
-  // console.log(categories)
-
-  const news = await newsCategoriesById("01")
-
-  // console.log(newsCategories)
-
-
-  return (
-    <div className="grid grid-cols-12 gap-4 w-[85%] mx-auto my-[60px]" >
-     
-
-     <div className=" col-span-3">
-
- <LeftSideBar categories={categories} activeId={null}></LeftSideBar>
-
-
-
-     </div>
-     <div className=" col-span-6">
-
-      <h1 className="text-xl font-bold">
-
-        All news
-        
-        </h1>     
-
-      <div className="space-y-4">
-
-      {
-        news.map(n => <div key={n._id} className="p-6 rounded-md border">
-
-          <h1>{n.title}</h1>
-        </div>)
-      }
-
-      </div>
-
-     </div>
-     <div className=" col-span-3">
-
-      <RightSideBar></RightSideBar>
-
-     </div>
-
-     
-    </div>
-  );
+redirect(`/category/${default_id}`)
 }
