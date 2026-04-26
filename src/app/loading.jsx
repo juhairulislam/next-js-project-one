5429eeffe7feb7da613c6c4ea23b84ac71fce37b
juhairulislam <juhairulislam2018@gmail.com>
2026-04-26 18:34:10 +0600

    add gloabal loading spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div className='h-[80vh] flex items-center justify-center'>
+            <span className="loading loading-spinner text-error"></span>
+
+        </div>
+    );
+};
+
+export default LoadingPage;
